20c14fec45e6e8d263a7a060b35843f0a5ccf04a
tingxuan zhang <zhangtx93@gmail.com>
2017-08-14 12:46:09 +0000

    finish static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  get 'static_pages/home' # Map requests for the URL /static_pages/home to the home action in the Static Pages controller
   get 'static_pages/help'
+  get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
